b1da94d5ac09a14436c264f3fc7a08f582bd7c87
Elvio <elviobg@gmail.com>
2018-05-14 11:07:17 -0300

    Revert "adiciona o código base do portal de noticias"
    This reverts commit 6526fb40364fe94730074971f624138a73678b8d.

--- portal/app.js
+++
@@ -1,9 +0,0 @@
-const app = require('./config/server');
-const db = require('./config/databaseConnection');
-
-const PORT = 3000
-
-//Start Server
-app.listen(PORT, function(){
-    console.log("Server running at port:", PORT);    
-});
--- portal/app/routes/admin.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app)
-{
-    app.get('/news/add', function(req, res){
-        res.render("admin/form_add_news");
-    });
-}
