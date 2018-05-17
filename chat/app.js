05a31e1b8a48fcc675ce23f693d0ced76eeabd3f
Elvio <elviobg@gmail.com>
2018-05-17 09:41:43 -0300

    Adiciona dependências para websocket

--- chat/app.js
+++ chat/app.js
@@ -2,6 +2,8 @@ const app = require('./config/server')
 const PORT = 3000
 
 // Start Server
-app.listen(PORT, function () {
+const server = app.listen(PORT, function () {
   console.log('Server running at port:', PORT)
 })
+
+require('socket.io').listen(server)
