8755ea1206ce1fd1f6e075ea1df28dc827825856
Elvio <elviobg@gmail.com>
2018-05-19 10:13:14 -0300

    Configuração inicial

---
+++ mmorpg/config/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const consign = require('consign')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+const path = require('path')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './app/views')
+app.use(express.static(path.join(__dirname, '/../app/public')))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(expressValidator())
+
+consign()
+  .include('app/routes')
+  .then('app/models')
+  .then('app/controllers')
+  .into(app)
+
+module.exports = app
