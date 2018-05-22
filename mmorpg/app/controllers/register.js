612a351d0c61e468f1af4193ff0fc1b24ee1fd37
Elvio <elviobg@gmail.com>
2018-05-21 23:29:33 -0300

    Rotas e Controllers de inserir usuario

--- mmorpg/app/controllers/register.js
+++ mmorpg/app/controllers/register.js
@@ -1,3 +1,20 @@
 module.exports.register = function (app, req, res) {
-  res.render('register')
+  res.render('register', {validation: {}, data: {}})
+}
+
+module.exports.registerUser = function (app, req, res) {
+  const dataForm = req.body
+  req.assert('name', 'Nome é obrigatorio').notEmpty()
+  req.assert('user', 'Usuário é obrigatorio').notEmpty()
+  req.assert('key', 'Senha é obrigatoria').notEmpty()
+  req.assert('house', 'Casa é obrigatoria').notEmpty()
+
+  const errors = req.validationErrors()
+  if (errors) {
+    // console.log(errors)
+    res.render('register', {validation: errors, data: dataForm})
+    return
+  }
+  
+  res.render('register', {validation: {}, data: {}})
 }
